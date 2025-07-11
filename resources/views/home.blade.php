a0b595347fca56b6a6440036d8fd7030b27a344f
Marco Antonio Guadalupe Rivas Guzman <marco.rivas@carsol.mx>
2025-07-10 21:16:59 -0600

    feat: Se agrego una vista de prueba para la proxima pagina del home

---
+++ resources/views/home.blade.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Atlantis</title>
+</head>
+<body>
+    <h1>Proyect Atlantis</h1>
+</body>
+</html>
